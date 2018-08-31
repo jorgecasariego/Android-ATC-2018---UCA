c92760da7ecd9e98657b2540d1c4389ea42d8b16
Jorge Casariego <jorgekasa@gmail.com>
2018-08-31 18:59:16 -0400

    Pororo parte 1 - SharedPreferences

---
+++ PororoPeliculas/app/src/main/java/me/jorgecasariego/pororopeliculas/PeliculasActivity.kt
@@ -0,0 +1,12 @@
+package me.jorgecasariego.pororopeliculas
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class PeliculasActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_peliculas)
+    }
+}
