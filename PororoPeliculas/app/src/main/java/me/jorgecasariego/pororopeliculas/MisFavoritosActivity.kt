1dc7b38877f9550c02284953ebd1e37442d5ab52
Jorge Casariego <jorgekasa@gmail.com>
2018-09-01 11:48:17 -0400

    Retrofit + Groupie + Menu

---
+++ PororoPeliculas/app/src/main/java/me/jorgecasariego/pororopeliculas/MisFavoritosActivity.kt
@@ -0,0 +1,12 @@
+package me.jorgecasariego.pororopeliculas
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MisFavoritosActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mis_favoritos)
+    }
+}
